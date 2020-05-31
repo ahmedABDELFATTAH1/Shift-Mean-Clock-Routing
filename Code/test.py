ed043966af7d7bcc23357d96b2bbd83b9895bada
Sofyan Mahmoud <sofyan1020@gmail.com>
2020-05-31 04:50:21 +0200

    Finish the Code

--- Code/test.py
+++
@@ -1,9 +0,0 @@
-size = input("Enter the number of nodes in the circuit: ")
-ArrOfArr = []
-for i in range(0,int(size)):
-    
-    x = int(input(("x of point %d: ") % (i+1)))
-    y = int(input(("y of point %d: ") % (i+1)))
-    ArrOfArr.append([x,y])
-
-print(ArrOfArr)
